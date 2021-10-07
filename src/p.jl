04ff2ab268a3553477f263388e1690b328f9ca01
Fran Urbano <viraltux@gmail.com>
2021-10-07 21:17:47 +0200

    assert NaN and and disallow missing values

--- src/p.jl
+++ src/p.jl
@@ -30,12 +30,13 @@ julia> isapprox(4*p(y)[end]/1000 , π, atol = 0.01)
 true
 ```
 """
-function p(dx::AbstractArray{<:Union{Missing,T}},
+function p(dx::AbstractArray{<:T},
            x0::AbstractArray{<:T} = reshape(repeat([0],size(dx,2)),1,:,1)) where T<:Real
 
     format_ol = "x0 format is Array{Real,3}(order, variable, lag)"
     @assert ndims(x0) <= 3 format_ol
-
+    @assert isnothing(findfirst(isnan, dx)) "NaN values not allowed"
+    
     ndims(x0) == 0 && (x0 = reshape([x0],1,1,1))
     ndims(x0) == 1 && (x0 = reshape(x0,:,1,1))
     ndims(x0) == 2 && (x0 = reshape(x0,:,size(x0,2),1))
@@ -48,13 +49,6 @@ function p(dx::AbstractArray{<:Union{Missing,T}},
     @assert nv == size(x0,2) format_ol
     @assert 0 <= la & la <= (n-1)
     
-    if !isnothing(findfirst(ismissing, dx))
-        @error "Missing values allowed at the start and end of `dx` but not within."
-        if !isnothing(findfirst(isnan, dx))
-            @error "NaN values not allowed; use `missing` at the start or end of `dx` but not within."
-        end
-    end
-
     px = similar(dx, nv == 1 ? (n+or*la,) : (n+or*la,nv))
     px[1:size(dx,1),:] = dx
     for i in or:-1:1
