a03d4aa125717fc6d312601b52d1aa5ef696a089
Fran Urbano <viraltux@gmail.com>
2021-10-07 21:18:20 +0200

    remove inlie

--- src/DataWrangler.jl
+++ src/DataWrangler.jl
@@ -1,21 +1,9 @@
 module DataWrangler
 
-println("⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠")
-println("⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠")
-println("⚠⚠⚠⚠                                                              ⚠⚠⚠⚠")
-println("⚠⚠⚠⚠     Package is on early development and not ready for use    ⚠⚠⚠⚠")
-println("⚠⚠⚠⚠                                                              ⚠⚠⚠⚠")
-println("⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠")
-println("⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠")
-
-
 using FFTW, Optim, Smoothers, Statistics
 
 include("impute.jl")
 export impute, impute!
-
-# include("inlie.jl")
-# export inlie, inlie!
 
 include("normalize.jl")
 export normalize, normalize!
@@ -36,7 +24,6 @@ Data transformations tools for analytics. The current available methods are:
     boxcox, iboxcox:         Box-Cox and inverse Box-Cox transformation
     d, p:                    Finite lagged difference and partial difference and its inverse
     impute, impute!:         Data imputation (loess inter/extra-polation, random local density)
-    inlie, inlie!:           Removal of outliers
     normalize, normalize!:   Data normalization (z-score, min-max, softmax, sigmoid)
 """
 DataWrangler
